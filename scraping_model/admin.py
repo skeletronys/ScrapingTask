6f025daf8f4caa015280e606019ed63353b136a7
skeletronys <mykytcey@gmail.com>
2025-05-16 13:02:25 +0200

    Added car to admin panel

--- scraping_model/admin.py
+++ scraping_model/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'price_usd', 'odometer', 'username',
+        'phone_number', 'car_number', 'car_vin', 'datetime_found'
+    )
+    search_fields = ('title', 'car_number', 'car_vin', 'username')
+    list_filter = ('price_usd',)
